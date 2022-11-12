45dc5196667892282c9d991d63dfe1cdec4367cf
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-11-12 23:07:37 +0100

    add response to wrong field input

--- Middlewares/error-handler.js
+++ Middlewares/error-handler.js
@@ -5,6 +5,11 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send(err.message);
   }
+  if (err.message === 'Unexpected field') {
+    return res
+      .status(400)
+      .send("please input 'csv' as the key in the input field");
+  }
   res.status(500).send('internal server error');
 };
 
